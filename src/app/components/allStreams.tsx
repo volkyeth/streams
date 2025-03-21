beea19e22ca12bd637b69636af9e8c03568a5a4d
Volky <volkyeth@pm.me>
2025-03-21 08:43:47 -0300

    reformat with prettier

--- src/app/components/allStreams.tsx
+++ src/app/components/allStreams.tsx
@@ -1,15 +1,13 @@
-import Link from 'next/link'
 import { cache } from 'react'
-import { createPublicClient, decodeFunctionData, http, parseAbiItem } from 'viem'
+import { createPublicClient, http, parseAbiItem } from 'viem'
+import { mainnet } from 'viem/chains'
+import { Log } from '../const/types'
+import Header from './header'
 import StreamRow from './streamRow'
 import UserStreams from './userStreams'
-import Header from './header'
-import { Log } from '../const/types'
-import { mainnet } from 'viem/chains'
 export const revalidate = 300 // revalidate the data at most every 5 minutes?
 
 const getData = cache(async () => {
-   console.log('fetchin data from viem...')
    const client = createPublicClient({
       chain: mainnet,
       transport: http(
@@ -28,7 +26,7 @@ const getData = cache(async () => {
    })
 
    let propIDs = []
-   for(const log of logs){
+   for (const log of logs) {
       const id = await client.getLogs({
          event: parseAbiItem('event ProposalExecuted(uint256 id)'),
          blockHash: log.blockHash,
@@ -48,7 +46,7 @@ const getData = cache(async () => {
          startTime: Number(l.args.startTime!),
          stopTime: Number(l.args.stopTime!),
          tokenAmount: Number(l.args.tokenAmount),
-         propID: Number((propIDs[i])[0].args.id),
+         propID: Number(propIDs[i][0].args.id),
       })
    }
    return returnLogs
